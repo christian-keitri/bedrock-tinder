cdd8d503ca84e934c537e324a64111e82561c3fa
Leonard Cruz <leonard.cruz@auto-guru.co.uk>
2025-04-03 17:02:58 +0800

    hot-reload function

--- auto-guru.co.uk/web/app/themes/autoguru/gulpfile.cjs
+++ auto-guru.co.uk/web/app/themes/autoguru/gulpfile.cjs
@@ -24,14 +24,21 @@ gulp.task('build-css', function () {
     .pipe(gulp.dest(paths.css.dest)); // Output the processed CSS
 });
 
+gulp.task('hot-reload', function (done) {
+  browserSync.reload(); // Reload browser after clearing cache
+});
+
+
+
 // Watch Task
 gulp.task('serve', function () {
   browserSync.init({
     proxy: 'http://localhost', // Replace with your local development URL
     notify: false,
+
   });
-  gulp.watch(paths.css.src, gulp.series('build-css')); // Watch for changes in CSS files
-  gulp.watch(paths.twig.src).on('change', browserSync.reload); // Reload browser on Twig changes
+  gulp.watch(paths.css.src, gulp.series('build-css', 'hot-reload')); // Watch for changes in CSS files
+  gulp.watch(paths.twig.src).on('change', gulp.series('build-css', 'hot-reload')); // Reload browser on Twig changes
 });
 
 gulp.task('default', gulp.series('build-css', 'serve')); // Default task to build and serve
