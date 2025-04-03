6f6e804e1a9d0720a5d22e6f441bd476b3666702
Leonard Cruz <leonard.cruz@auto-guru.co.uk>
2025-04-03 16:36:44 +0800

    Working dev setup

---
+++ auto-guru.co.uk/web/app/themes/autoguru/gulpfile.cjs
@@ -0,0 +1,37 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+// Paths
+const paths = {
+  css: {
+    src: 'static/css/tailwind.css',
+    dest: './',
+  },
+  twig: {
+    src: 'views/**/*.twig', // Path to all Twig files
+  }
+};
+
+// PostCSS Task
+gulp.task('build-css', function () {
+  return gulp
+    .src(paths.css.src)
+    .pipe(sourcemaps.init()) // Initialize sourcemaps
+    .pipe(postcss()) // Process CSS with PostCSS
+    .pipe(sourcemaps.write('.')) // Write sourcemaps to the same directory
+    .pipe(gulp.dest(paths.css.dest)); // Output the processed CSS
+});
+
+// Watch Task
+gulp.task('serve', function () {
+  browserSync.init({
+    proxy: 'http://localhost', // Replace with your local development URL
+    notify: false,
+  });
+  gulp.watch(paths.css.src, gulp.series('build-css')); // Watch for changes in CSS files
+  gulp.watch(paths.twig.src).on('change', browserSync.reload); // Reload browser on Twig changes
+});
+
+gulp.task('default', gulp.series('build-css', 'serve')); // Default task to build and serve
