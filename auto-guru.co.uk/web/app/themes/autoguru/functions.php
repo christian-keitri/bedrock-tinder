a45d8be707e0780281ab705466eaa75628cb222b
Leonard Cruz <leonard.cruz@auto-guru.co.uk>
2025-04-07 15:38:04 +0800

    Added hero component and custom fields

--- auto-guru.co.uk/web/app/themes/autoguru/functions.php
+++ auto-guru.co.uk/web/app/themes/autoguru/functions.php
@@ -9,9 +9,27 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 require_once __DIR__ . '/src/autoguru.php';
 
+use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
 Timber\Timber::init();
 
 // Sets the directories (inside your theme) to find .twig files.
 Timber::$dirname = [ 'templates', 'views' ];
 
 new AutoGuruTheme();
+
+add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
+
+function crb_attach_theme_options() {
+  Container::make( 'theme_options', __( 'Theme Options' ) )
+    ->add_fields( array(
+      Field::make( 'text', 'crb_text', 'Text Field' ),
+    ) );
+}
+
+add_action('after_setup_theme', 'crb_load');
+function crb_load() {
+  require_once __DIR__ . '/vendor/autoload.php';
+  \Carbon_Fields\Carbon_Fields::boot();
+}
